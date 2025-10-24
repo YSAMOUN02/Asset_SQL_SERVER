26f8222e9e59d572cf6ae1094eb3f33e6e6b25a5
YSAMOUN02 <ysamoun2@gmail.com>
2025-10-24 12:07:57 +0700

    sadad

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -432,7 +432,7 @@
                                 </li>
                             @endif
                             <li>
-                                <a href="/admin/assets-ownership/1">
+                                <a href="/admin/assets-ownership/0">
                                     <button type="button"
                                         class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
 
